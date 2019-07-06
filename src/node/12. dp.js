79147c876ce4308574cba16769ea3aa57b4f3e94
donaldcao <donggecao@gmail.com>
2019-07-06 22:04:38 +0800

    add dynamic programming

---
+++ src/node/12. dp.js
@@ -0,0 +1,33 @@
+
+function MinCoinChange(coins) {
+    var coins = coins;  // {1}
+    var cache = [];   // {2}
+
+    this.makeChange = function(amount) {
+        var me  = this;
+        if(!amount) {   // {3}
+            return [];
+        }
+
+        if(cache[amount]) {   // {4}
+            return cache[amount];
+        }
+
+        var min = [], newMin, newAmount;
+        for(var i = 0; i < coins.length; i++) {  // {5}
+            var coin = coins[i];
+            newAmount = amount - coin; // {6}
+            if(newAmount >= 0) {
+                newMin = me.makeChange(newAmount); // {7}
+            }
+            if(newAmount >= 0 &&  // {8}
+                (newMin.length < min.length-1 || !min.length)  // {9}
+                && (newMin.length || !newAmount))   //{10}
+                {
+                    min = [coin].concat(newMin); // {11}
+                    console.log('new Min ' + min + ' for ' + amount);
+                }
+        }
+        return (cache[amount] = min); // {12}
+    };
+}
