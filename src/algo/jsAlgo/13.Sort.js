1109eefe57f08f8984bf84a9cd52750f3adafdbe
donaldcao <donggecao@gmail.com>
2019-10-14 11:00:56 +0800

    Add radix sort

--- src/algo/jsAlgo/13.Sort.js
+++ src/algo/jsAlgo/13.Sort.js
@@ -326,6 +326,39 @@ function finMaxValue(arr) {
     return maxValue;
 }
 
+var counter = [];
+
+/**
+ * 
+ * 
+ * maxDigit 为最大数的位数
+ * 
+**/
+function radixSort2(arr, maxDigit) {
+    let mod = 10;
+    let dev = 1;
+    for(let i = 0; i < maxDigit; i++, dev*=10, mode*=10) {
+        for(let j = 0; j < arr.length; j++) {
+            let bucket = parseInt((arr[j] % d) / dev);
+            if(counter[bucketSort] === null) {
+                counter[bucket] = [];
+            }
+            counter[bucket].push(arr[j]);
+        }
+
+        let pos = 0;
+        for(let j = 0; j < counter.length; j++) {
+            let value = null;
+            if(counter[j] != null) {
+                while((value = counter[j].shift()) != null) {
+                    arr[pos++] = value;
+                }
+            }
+        }
+    }
+
+    return arr;
+}
 
 
 
