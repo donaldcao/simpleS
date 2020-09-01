a8d8ad397421b9cc70d8aaa056c4847a73138406
Cao <dongge.cao@siemens-healthineers.com>
2020-09-01 10:05:42 +0800

    Refine ticTacToc algo

--- src/algo/leetcode/0348_TicTacToe.cpp
+++ src/algo/leetcode/0348_TicTacToe.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 class TicTacToe {
-    public:
-        TicTacToe(int n): rows(n), cols(n), N(n), diag(0), rev_diag(0) {}
-
-        int move(int row, int col, int player) {
-            int add = player == 1 ? 1 : -1;
-            rows[row] += add;
-            cols[col] += add;
-            diag += (row == col ? add : 0);
-            rev_diag += (row == N-col-1 ? add : 0);
-            return (abs(rows[row]) == N || abs(cols[col]) == N || abs(diag) == N || abs(rev_diag == N)) ? player : 0;
-        }
     
+    public:
+        TicTacToe(int n) : rows(n), cols(n), N(n), diag(0), rev_diag(0) {}
+
+        // player number is 1 or 2, add == 1 if player is 1, or add == -1 if player is 2.
+        int move(int row, int col, int player) {
+            int add = (player == 1 ? 1 : -1);
+            rows[row] += add, cols[col] += add;
+            diag += (row == col ? add : 0), rev_diag += (row + col == N -1 ? add : 0);
+            return (abs(rows[row]) == N || abs(cols[col]) == N || abs(diag) == N || abs(rev_diag) == N) ? player : 0;
+        }
+
     private:
         vector<int> rows, cols;
         int diag, rev_diag, N;
+
 };
