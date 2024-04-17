6ae86887627c7671bfc3cac84d798ca4e2336ef8
Donald Cao <donggecao@gmail.com>
2024-04-17 17:46:32 +0800

    check endian.

--- src/c/check_endian.c
+++ src/c/check_endian.c
@@ -1,5 +1,18 @@
+#include <stdio.h>
 
 int main() {
+    union union_bytes
+    {
+        int num;
+        char arr[2];
+
+    } u_bytes = {
+        .num = 0x0102,
+    };
+    
+    if(u_bytes.arr[0] == 2){
+        printf("%s\n", "小端");
+    }
 
 
     return 0;
